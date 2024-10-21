0e4e9d90ba85cdc39ec74bfab91345a473ef24a4
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2024-10-21 22:00:05 +0700

    UTS

---
+++ app/src/main/java/com/nashwa/utsnashwami2b/model/listModel.kt
@@ -0,0 +1,12 @@
+package com.nashwa.utsnashwami2b.model
+
+data class listModel(
+
+
+    var NamaMkn : String,
+    var ImageMkn : Int,
+    var tglMkn : String,
+    var hargaMkn : String,
+    var totalMkn : String,
+
+)
